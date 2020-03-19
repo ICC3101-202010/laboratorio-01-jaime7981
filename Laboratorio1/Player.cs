2eedd454d605322b99dbdee122aabf92d85478a6
jaime7981 <39995316+jaime7981@users.noreply.github.com>
2020-03-19 09:31:00 -0300

    Primer upload; Error con los números random

---
+++ Laboratorio1/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    class Player
+    {
+        private string firstName;
+        private string secondName;
+
+        private int min;
+        private int max;
+        private int rand;
+
+        public Player(string fisrtName, string secondName)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        public void Trhow()
+        {
+            rand = RandomNumber(0, 3);
+            Console.WriteLine(rand);
+        }
+
+        // Generate a random number between two numbers  
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+    }
+}
---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player("Bob", "Kunga");
+            for (int i=0; i < 4; ++i) { 
+            p1.Trhow();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
